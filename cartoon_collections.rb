927b18ac951c081dee771e1c1e59f388efb099cb
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-10-07 23:15:43 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array) # code an argument here
   # Your code here
+  array = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  array.each_with_index do |item, index|
+    puts "#{index+1} #{item}
+  end
 end
 
 def summon_captain_planet# code an argument here
